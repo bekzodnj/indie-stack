d5e74359fdbefddeaf8129ff247dfdbfa1bc8ceb
bekzodnj <bekzodnx@gmail.com>
2024-11-26 23:31:57 +0100

    Fixed: parent route thingy

--- app/routes.ts
+++ app/routes.ts
@@ -1,20 +1,16 @@
-import {
-  type RouteConfig,
-  route,
-  layout,
-  index,
-} from "@react-router/dev/routes";
+import { type RouteConfig, route, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/_index.tsx"),
   route("login", "routes/login.tsx"),
 
-  layout("routes/notes.tsx", [
+  route("notes", "routes/notes.tsx", [
     index("./routes/notes.home.tsx"),
     route("new", "routes/notes.new.tsx"),
     route(":noteId", "routes/notes.$noteId.tsx"),
   ]),
 
   route("logout", "routes/logout.tsx"),
+  route("join", "routes/join.tsx"),
   route("healthcheck", "routes/healthcheck.tsx"),
 ] satisfies RouteConfig;
